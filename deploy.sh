cfd34076b08b6edaa31f4a2d8e03a61e4aaed489
Jimbau04 <diego.jimbau04@gmail.com>
2025-12-04 22:35:03 -0600

    add(deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-gcloud app deploy --project pik-api
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
+gunicorn dev_sistema_escolar_api.wsgi:application --bind
